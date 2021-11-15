dc22e565c13abdc4d04c7d0471f6f7b8f5e71d11
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-11-15 23:04:24 +0300

    create project Timer

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React, {useState, useRef} from 'react'
+
+const Timer = () => {
+
+
+  return (
+    <div className="timer">
+      <button className="reset"onClick={() => {}}></button>
+    </div>
+  )
+} 
+
+export default Timer;
+
